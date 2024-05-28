03dc3adbda0def491c86025105b3a0b6eb99f7e8
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2024-05-28 23:58:21 +0300

    Update user model

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from users.views import UserViewSet
+
+router = DefaultRouter()
+router.register(r"users", UserViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
 ]
+
+urlpatterns += router.urls
